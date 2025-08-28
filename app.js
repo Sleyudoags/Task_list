d6634d76fa00b92a9fdc5172e1d671239cc2facf
Sleyudoags <kudoags@gmail.com>
2025-08-28 16:44:57 +0100

    add and remove

---
+++ app.js
@@ -0,0 +1,66 @@
+// DEFINE UI VARS
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task'); 
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners 
+function loadEventListeners( ){
+  //add task event 
+  form.addEventListener('submit', addTask);
+  //remove task event
+  taskList.addEventListener('click', removeTask);
+//cler tasks event 
+clearBtn.addEventListener('click', clearTasks);
+}
+
+//Add Task 
+function addTask(e) {
+  if(taskInput.value === '') { 
+    alert('Add  a task')
+  }
+
+  //create li element 
+  const li = document.createElement('li');
+  //add class 
+  li.className = 'collection-item';
+  // create text node and append to li 
+  li.appendChild(document.createTextNode(taskInput.value));
+  //create new link element
+  const link = document.createElement('a');
+  //add class 
+  link.className = 'delete-item secondary-content';
+  //add icon html
+  link.innerHTML  = '<i class="fa fa-remove"></i>';
+  //append the link to li
+  li.appendChild(link);
+
+  // append li to ul 
+  taskList.appendChild(li);
+
+  //clear input 
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+//remove task 
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item'))
+   {
+    if (confirm('Are You Sure')){
+      e.target.parentElement.parentElement.remove();
+    }
+   }
+}
+
+//clear  tasks 
+function clearTasks(){
+  taskLists.innerHTML='';
+}
+
+
